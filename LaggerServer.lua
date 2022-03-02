55ffb3db44552aab53270b14d6248a17966f30fb
OceanTubez <84045281+OceanTubez@users.noreply.github.com>
2022-03-02 10:48:39 -0800

    Created LaggerServer script for roblox studio.
    Check out LaggerClient, in-order to initialize this.

---
+++ LaggerServer.lua
@@ -0,0 +1,31 @@
+--[[
+
+This code was created by OceanTubez, To be used as a lag crasher.
+Insert this script in ServerScriptService.
+
+Insert another LOCALSCRIPT inside StarterGui
+Then check out LaggerClient.lua
+
+--]]
+
+--// Services \\--
+local ServerStorage = game:GetService("ServerStorage")
+local event = Instance.new("RemoteEvent", ServerStorage)
+event.Name = "Crash"
+local Workspace = game.Workspace
+
+--// Variables \\--
+local intensity = 5
+
+--// Code \\--
+
+game.Players.PlayerAdded:Connect(function(plr)
+    plr.CharacterAdded:Connect(function(char)
+        
+        local val = Instance.new("BoolValue", char)
+        val.Name = "Crash"
+        
+        event:FireClient(plr, intensity)
+        
+    end)
+end)
